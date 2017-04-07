164071e084b158b2e5035760a649070fdfc6035b
MAL315 <MAL315@users.noreply.github.com>
2017-04-06 20:21:57 -0400

    Removed bkwallet link

--- footer.php
+++ footer.php
@@ -18,7 +18,6 @@
 					<ul>
 						<li><a href="http://wallet.peercointalk.org/">Wallet Generator (Beta!)</a></li>
 						<li><a href="http://ppc.blockr.io/">Blockr.io - Block Explorer</a></li>
-						<li><a href="http://bkchain.org/ppc">bkchain.org - Block Explorer</a></li>
 						<li><a href="http://bitinfocharts.com/comparison/hashrate-ppc.html">Network Statistics</a></li>
 						<li><a href="http://www.coinwarz.com/calculators/peercoin-mining-calculator/">Mining Calculator</a></li>
 					</ul>
