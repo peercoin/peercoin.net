1efbfd19b7ceeb75a8570cd58350a1f541e434dd
MAL315 <MAL315@users.noreply.github.com>
2017-10-25 06:12:06 -0400

    Update id.php

--- locale/languages/id.php
+++ locale/languages/id.php
@@ -228,7 +228,7 @@ $lang['id'] += array(
 $lang['id'] += array(
 	// Wallet
 	'wallet.title' => 'Dompet',
-	'wallet.desc' => 'Sebuah dompet Peercoin adalah aplikasi yang digunakan untuk menyimpan dan bertransaksi Peercoins dengan pengguna lain. Peercoin memiliki dua dompet desktop yaitu: Peerunity dan Peercoin-QT. Kami merekomendasikan Peerunity untuk sebagian besar pengguna. Untuk informasi lebih lanjut tentang cara menggunakan software dompet Anda, kunjungi  <a href="newcomer">Halaman Pendatang Baru</a>.',
+	'wallet.desc' => 'Sebuah dompet Peercoin adalah aplikasi yang digunakan untuk menyimpan dan bertransaksi Peercoins dengan pengguna lain. Untuk informasi lebih lanjut tentang cara menggunakan software dompet Anda, kunjungi  <a href="newcomer">Halaman Pendatang Baru</a>.',
 	'wallet.peercoinqt' => 'Peercoin-QT<br>(core protocol)',
 	'wallet.peercoinqt_desc' => 'Peercoin-QT adalah protokol inti Peercoin, yang dibangun dan dikelola oleh Peercoin pengembang Sunny King. Hanya menerima pembaruan prioritas tinggi.',
 	'wallet.paper_wallet' => 'Dompet Kertas.',
