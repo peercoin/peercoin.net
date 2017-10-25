9ab78395936a3a1329290c41d88124228cac917b
MAL315 <MAL315@users.noreply.github.com>
2017-10-25 06:47:17 -0400

    Update en.php

--- locale/languages/en.php
+++ locale/languages/en.php
@@ -243,7 +243,7 @@ $lang['en'] += array(
 	'wallet.title' => 'Wallets',
 	'wallet.desc' => 'A Peercoin wallet is an application used to store and transact Peercoins with other users. For more information on how to use your wallet software, visit the <a href="newcomers">Newcomers page</a>.',
 	'wallet.peercoinqt' => 'Peercoin-QT<br>(core protocol)',
-	'wallet.peercoinqt_desc' => 'Peercoin-QT is Peercoin’s core protocol, built and maintained by Peercoin developer Sunny King. It receives only high-priority updates.',
+	'wallet.peercoinqt_desc' => 'peercoin-qt is Peercoin\'s official wallet.',
 	'wallet.paper_wallet' => 'Paper Wallet',
 	'wallet.paper_wallet_desc' => 'A paper wallet stays completely offline, printed onto on a piece of paper for safekeeping. Use a paper wallet if you want to limit access to your Peercoins, and plan to store them for a long period of time.',
 	'wallet.android_wallet' => 'Android Wallet',
