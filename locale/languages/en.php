02c5934e64eb87c071097409b58d1342ea34feca
Josh <josh.wilburn@protonmail.com>
2018-05-13 19:58:23 -0500

    adding foundation splash page

--- locale/languages/en.php
+++ locale/languages/en.php
@@ -126,6 +126,7 @@ $lang['en'] += array(
 	'header.resources' => 'Resources',
 	'header.community' => 'Community',
 	'header.languages' => 'Languages',
+	'header.foundation' => 'Foundation',
 );
 
 $lang['en'] += array(
@@ -291,4 +292,13 @@ $lang['en'] += array(
 	'mineguide.step2_multipoollist' => 'Multicoin pools:',
 	'mineguide.step3' => 'Sign up with the pool',
 	'mineguide.step3_desc' => 'Follow the instructions on the pool\'s website. You can also find more pools <a href="https://wiki.peercointalk.org/index.php?title=List_of_services#Mining_Pools">here</a>.',
+);
+
+$lang['en'] += array(
+	'foundation.mission_statement' => 'Mission Statement',
+	'foundation.mission_statement_desc' => "The Peercoin Foundation has been established with the simple mission of promoting and supporting the continued education, development, and overall progression of the Peercoin project. We seek to empower future Peercoin team members by providing the tools necessary to perpetuate Peercoin's long standing reputation for bringing world-first innovations to the Blockchain.",
+	'foundation.foundation_text' => 'Peercoin Foundation',
+	'foundation.community_support' => 'Community Support',
+	'foundation.community_support_desc' => 'If you would like to get involved with Peercoin, or would simply like to support the team, there are a handful of options at your disposal. If you are interested in becoming a developer for Peercoin or Peercoin related projects such as PeerAssets, send us an email at <a href="mailto:foundation@peercoin.net">foundation@peercoin.net</a>. If you would like to donate PPC to the Peercoin Foundation to support the expansion of a project, simply send your PPC donation to the multisignature address below.',
+	
 );
