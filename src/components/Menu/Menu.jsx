a58dee31f9b2a77c85cc8e821ec02221a968156c
Claudio Holanda <claudiohw@gmail.com>
2019-08-09 15:29:44 -0300

    Adding fixed menu functionality

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,69 +1,86 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Menu.scss';
 
 function Menu() {
+  const [isFixed, setIsFixed] = useState(false);
+  
+  useEffect(() => {
+    let heroHeight = 100;
+
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > heroHeight) {
+        setIsFixed(true);
+      } else {
+        setIsFixed(false);
+      }
+    });
+  }, []);
+
   return (
-    <nav className="menu">
-      <ul className="menu__main">
-        <li>Home</li>
-        <li>
-          Learn
-          <img src="/img/icon-menu-arrow-down.svg" alt=""/>
-          <ul className="menu__main__submenu">      
-            <li><a href="https://peercoin.github.io/PeercoinUniversity/" class="submenu">Peercoin University</a></li>
-            <li><a href="https://peercoin.github.io/PeercoinUniversity/#/2-what-is-a-blockchain-" class="submenu">What is a Blockchain?</a></li>
-            <li><a href="https://peercoin.github.io/PeercoinUniversity/#/6-centralization-of-bitcoin" class="submenu">Inherent Centralization of Proof-of-Work Blockchains</a></li>
-            <li><a href="https://peercoin.github.io/PeercoinUniversity/#/9-peercoin-proof-of-stake-consensus" class="submenu">Peercoin &amp; Proof-of-Stake Consensus</a></li>
-            <li><a href="https://peercoin.github.io/PeercoinUniversity/#/10-first-efficient-sustainable-blockchain" class="submenu">Efficient &amp; Sustainable Security</a></li>
-            <li><a href="https://peercoin.github.io/PeercoinUniversity/#/11-economics-of-peercoin" class="submenu">Economics of Peercoin</a></li>
-            <li><a href="https://peercoin.github.io/PeercoinUniversity/#/12-scalability-of-peercoin-" class="submenu">Scalability of Peercoin</a></li>
-            <li><a href="https://medium.com/peercoin/deflation-and-cryptocurrencies-the-long-term-ramifications-of-fixed-supply-currencies-c192f44dddc1" class="submenu">Deflation and Cryptocurrencies</a></li>
-            <li><a href="https://medium.com/peercoin/peercoins-hybrid-chain-solution-to-proof-of-work-global-scaling-and-environmental-impact-c36757e89d6e" class="submenu">Peercoin's Hybrid Chain Solution</a></li>
-            <li><a href="https://medium.com/peercoin/the-rich-get-richer-concept-in-proof-of-stake-systems-82c5ceeff326" class="submenu">"Rich get Richer" Fallacy</a></li>
-            <li><a href="https://docs.peercoin.net/#/frequently-asked-questions" class="submenu">Frequently Asked Questions</a></li>
-          </ul>
-        </li>
-        <li>Get Started</li>
-        <li>
-          Resources
-          <img src="/img/icon-menu-arrow-down.svg" alt=""/>
-          <ul className="menu__main__submenu">
-            <li><a href="https://docs.peercoin.net/" class="submenu"><img src="/img/icons/docs_green.png" alt="" />Documentation</a></li>
-            <li><a href="/resources#mining" class="submenu"><img src="/img/icons/pickaxe_green.png" alt="" />Mining</a></li>
-            <li><a href="/resources#university" class="submenu"><img src="/img/icons/graduationcap.png" alt="" />Peercoin University</a></li>
-            <li><a href="/resources#exchanges" class="submenu"><img src="/img/icons/exchange_green.png" alt="" />Exchanges</a></li>
-            <li><a href="/resources#blockexplorers" class="submenu"><img src="/img/icons/block_green.png" alt="" />Block Explorers</a></li>
-            <li><a href="/resources#whitepaper" class="submenu"><img src="/img/icons/whitepaper_green.png" alt="" />Whitepaper</a></li>
-            <li><a href="/resources#graphics" class="submenu"><img src="/img/icons/graphics_green.png" alt="" />Graphics</a></li>
-          </ul>
-        </li>
-        <li>
-          Project
-          <img src="/img/icon-menu-arrow-down.svg" alt=""/>
-          <ul className="menu__main__submenu">
-            <li><a href="https://github.com/peercoin/" class="submenu"><img src="/img/icons/github_green.png" alt="" />GitHub</a></li>
-            <li><a href="projects.html" class="submenu"><img src="/img/icons/projects_green.png" alt="" />Projects</a></li>
-            <li><a href="https://medium.com/peercoin" class="submenu"><img src="/img/icons/blog_green.png" alt="" />Blog</a></li>
-            <li><a href="foundation.html" class="submenu"><img src="/img/icons/foundation_green.png" alt="" />Peercoin Foundation</a></li>
-            <li><a href="contactus.html" class="submenu"><img src="/img/icons/contact_green.png" alt="" />Contact Us</a></li>
-          </ul>
-        </li>
-        <li>Wallet</li>
-        <li>
-          Community
-          <img src="/img/icon-menu-arrow-down.svg" alt=""/>
-          <ul className="menu__main__submenu">
-            <li><a href="https://talk.peercoin.net/" class="submenu"><img src="/img/icons/forum_green.png" alt="" />Forum</a></li>
-            <li><a href="https://twitter.com/PeercoinPPC" class="submenu"><img src="/img/icons/twitter_green.png" alt="" />Twitter</a></li>
-            <li><a href="https://www.facebook.com/Peercoin/" class="submenu"><img src="/img/icons/facebook_green.png" alt="" />Facebook</a></li>
-            <li><a href="https://www.reddit.com/r/peercoin" class="submenu"><img src="/img/icons/reddit_green.png" alt="" />Reddit</a></li>
-            <li><a href="https://t.me/peercoin" class="submenu"><img src="/img/icons/telegram_green.png" alt="" />Telegram</a></li>
-            <li><a href="https://discord.gg/XPxfwtG" class="submenu"><img src="/img/icons/discord_green.png" alt="" />Discord</a></li>
-            <li><a href="https://www.youtube.com/user/PeerCoin" class="submenu"><img src="/img/icons/youtube_green.png" alt="" />Youtube</a></li>
-            <li><a href="https://bitcointalk.org/index.php?topic=101820.0" class="submenu"><img src="/img/icons/bitcoinchat_green.png" alt="" />Bitcointalk</a></li>
-          </ul>
-        </li>
-      </ul>
+    <nav className={isFixed ? 'menu menu--fixed' : 'menu'}>
+      <div className="container">
+        {isFixed && <img src="/img/logo-menu-white.svg" alt="Peercoin" className="menu__logo" />}
+        <ul className="menu__main">
+          <li>Home</li>
+          <li>
+            Learn
+            <img src="/img/icon-menu-arrow-down.svg" alt=""/>
+            <ul className="menu__main__submenu">      
+              <li><a href="https://peercoin.github.io/PeercoinUniversity/" className="submenu">Peercoin University</a></li>
+              <li><a href="https://peercoin.github.io/PeercoinUniversity/#/2-what-is-a-blockchain-" className="submenu">What is a Blockchain?</a></li>
+              <li><a href="https://peercoin.github.io/PeercoinUniversity/#/6-centralization-of-bitcoin" className="submenu">Inherent Centralization of Proof-of-Work Blockchains</a></li>
+              <li><a href="https://peercoin.github.io/PeercoinUniversity/#/9-peercoin-proof-of-stake-consensus" className="submenu">Peercoin &amp; Proof-of-Stake Consensus</a></li>
+              <li><a href="https://peercoin.github.io/PeercoinUniversity/#/10-first-efficient-sustainable-blockchain" className="submenu">Efficient &amp; Sustainable Security</a></li>
+              <li><a href="https://peercoin.github.io/PeercoinUniversity/#/11-economics-of-peercoin" className="submenu">Economics of Peercoin</a></li>
+              <li><a href="https://peercoin.github.io/PeercoinUniversity/#/12-scalability-of-peercoin-" className="submenu">Scalability of Peercoin</a></li>
+              <li><a href="https://medium.com/peercoin/deflation-and-cryptocurrencies-the-long-term-ramifications-of-fixed-supply-currencies-c192f44dddc1" className="submenu">Deflation and Cryptocurrencies</a></li>
+              <li><a href="https://medium.com/peercoin/peercoins-hybrid-chain-solution-to-proof-of-work-global-scaling-and-environmental-impact-c36757e89d6e" className="submenu">Peercoin's Hybrid Chain Solution</a></li>
+              <li><a href="https://medium.com/peercoin/the-rich-get-richer-concept-in-proof-of-stake-systems-82c5ceeff326" className="submenu">"Rich get Richer" Fallacy</a></li>
+              <li><a href="https://docs.peercoin.net/#/frequently-asked-questions" className="submenu">Frequently Asked Questions</a></li>
+            </ul>
+          </li>
+          <li>Get Started</li>
+          <li>
+            Resources
+            <img src="/img/icon-menu-arrow-down.svg" alt=""/>
+            <ul className="menu__main__submenu">
+              <li><a href="https://docs.peercoin.net/" className="submenu"><img src="/img/icons/docs_green.png" alt="" />Documentation</a></li>
+              <li><a href="/resources#mining" className="submenu"><img src="/img/icons/pickaxe_green.png" alt="" />Mining</a></li>
+              <li><a href="/resources#university" className="submenu"><img src="/img/icons/graduationcap.png" alt="" />Peercoin University</a></li>
+              <li><a href="/resources#exchanges" className="submenu"><img src="/img/icons/exchange_green.png" alt="" />Exchanges</a></li>
+              <li><a href="/resources#blockexplorers" className="submenu"><img src="/img/icons/block_green.png" alt="" />Block Explorers</a></li>
+              <li><a href="/resources#whitepaper" className="submenu"><img src="/img/icons/whitepaper_green.png" alt="" />Whitepaper</a></li>
+              <li><a href="/resources#graphics" className="submenu"><img src="/img/icons/graphics_green.png" alt="" />Graphics</a></li>
+            </ul>
+          </li>
+          <li>
+            Project
+            <img src="/img/icon-menu-arrow-down.svg" alt=""/>
+            <ul className="menu__main__submenu">
+              <li><a href="https://github.com/peercoin/" className="submenu"><img src="/img/icons/github_green.png" alt="" />GitHub</a></li>
+              <li><a href="projects.html" className="submenu"><img src="/img/icons/projects_green.png" alt="" />Projects</a></li>
+              <li><a href="https://medium.com/peercoin" className="submenu"><img src="/img/icons/blog_green.png" alt="" />Blog</a></li>
+              <li><a href="foundation.html" className="submenu"><img src="/img/icons/foundation_green.png" alt="" />Peercoin Foundation</a></li>
+              <li><a href="contactus.html" className="submenu"><img src="/img/icons/contact_green.png" alt="" />Contact Us</a></li>
+            </ul>
+          </li>
+          <li>Wallet</li>
+          <li>
+            Community
+            <img src="/img/icon-menu-arrow-down.svg" alt=""/>
+            <ul className="menu__main__submenu">
+              <li><a href="https://talk.peercoin.net/" className="submenu"><img src="/img/icons/forum_green.png" alt="" />Forum</a></li>
+              <li><a href="https://twitter.com/PeercoinPPC" className="submenu"><img src="/img/icons/twitter_green.png" alt="" />Twitter</a></li>
+              <li><a href="https://www.facebook.com/Peercoin/" className="submenu"><img src="/img/icons/facebook_green.png" alt="" />Facebook</a></li>
+              <li><a href="https://www.reddit.com/r/peercoin" className="submenu"><img src="/img/icons/reddit_green.png" alt="" />Reddit</a></li>
+              <li><a href="https://t.me/peercoin" className="submenu"><img src="/img/icons/telegram_green.png" alt="" />Telegram</a></li>
+              <li><a href="https://discord.gg/XPxfwtG" className="submenu"><img src="/img/icons/discord_green.png" alt="" />Discord</a></li>
+              <li><a href="https://www.youtube.com/user/PeerCoin" className="submenu"><img src="/img/icons/youtube_green.png" alt="" />Youtube</a></li>
+              <li><a href="https://bitcointalk.org/index.php?topic=101820.0" className="submenu"><img src="/img/icons/bitcoinchat_green.png" alt="" />Bitcointalk</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
--- src/pages/Index/Index.jsx
+++ src/pages/Index/Index.jsx
@@ -13,8 +13,48 @@ function Index() {
         The world's first efficient and sustainable public blockchain, serving as a secure base layer and cryptocurrency for the future blockchain connected world.
       </p>
       <div className="hero__actions">
-        <a href="#" className="hero__actions__action">Get Started</a>
-        <a href="#" className="hero__actions__action hero__actions__action--white">What is Peercoin?</a>
+        <a href="/get-started" className="hero__actions__action">Get Started</a>
+        <a href="/what-is-peercoin" className="hero__actions__action hero__actions__action--white">What is Peercoin?</a>
+      </div>
+    </div>
+    <div className="main">
+      <div className="container">
+      <section className="home-description">
+          <div className="home-description__text">
+            <h2 className="home-description__text__title">Efficient Security</h2>
+            <p>Peercoin (PPC) first came online in 2012, making it one of the earliest pioneering blockchains. The key innovation of Peercoin is its invention of proof-of-stake, an alternative consensus protocol to Bitcoin’s proof-of-work.</p>
+            <p>Proof-of-work blockchains are secured by proving the consumption of a costly limited resource: electricity. Proof-of-stake replaces this expensive security protocol by utilizing an alternative scarce resource: time.</p>
+            <p>Due to the cost efficient nature of proof-of-stake’s time based consensus rules, Peercoin is capable of allowing any network connected computer to participate in the blockchain’s security process. This efficiency strengthens Peercoin by growing the number of security providers and ensuring that security can be sustained over the long-term.</p>
+          </div>
+          <img className="home-description__img" src="/img/animations/efficient-and-sustainable.svg" alt=""/>
+        </section>
+        <section className="home-description">
+          <img className="home-description__img" src="/img/animations/user-driven-governance.svg" alt=""/>
+          <div className="home-description__text home-description__text--left">
+            <h2 className="home-description__text__title">User Governance</h2>
+            <p>In Peercoin, coin owners (stakeholders) are the ones who wield influence over the network, produce new blocks, and secure the chain. Stakeholders of Peercoin co-own the blockchain and collectively decide its future through protocol voting.</p>
+            <p>Voting is carried out the same way Peercoin is secured, through a process called proof-of-stake minting. Stakeholders participate in minting simply by installing the version of Peercoin they support, loading their wallet with PPC, and holding them while they are occasionally selected by Peercoin’s protocol to mint the next block in the chain. Every minted block rewards stakeholders with new PPC and a vote.</p>
+            <p>This makes Peercoin the first blockchain capable of allowing its protocol rules to be governed directly by its users, making for a network that is more decentralized, democratic, and easily secured by people around the world.</p>
+          </div>
+        </section>
+        <section className="home-description">
+          <div className="home-description__text">
+            <h2 className="home-description__text__title">Efficient Security</h2>
+            <p>Peercoin (PPC) first came online in 2012, making it one of the earliest pioneering blockchains. The key innovation of Peercoin is its invention of proof-of-stake, an alternative consensus protocol to Bitcoin’s proof-of-work.</p>
+            <p>Proof-of-work blockchains are secured by proving the consumption of a costly limited resource: electricity. Proof-of-stake replaces this expensive security protocol by utilizing an alternative scarce resource: time.</p>
+            <p>Due to the cost efficient nature of proof-of-stake’s time based consensus rules, Peercoin is capable of allowing any network connected computer to participate in the blockchain’s security process. This efficiency strengthens Peercoin by growing the number of security providers and ensuring that security can be sustained over the long-term.</p>
+          </div>
+          <img className="home-description__img" src="/img/animations/efficient-and-sustainable.svg" alt=""/>
+        </section>
+        <section className="home-description">
+          <img className="home-description__img" src="/img/animations/user-driven-governance.svg" alt=""/>
+          <div className="home-description__text home-description__text--left">
+            <h2 className="home-description__text__title">User Governance</h2>
+            <p>In Peercoin, coin owners (stakeholders) are the ones who wield influence over the network, produce new blocks, and secure the chain. Stakeholders of Peercoin co-own the blockchain and collectively decide its future through protocol voting.</p>
+            <p>Voting is carried out the same way Peercoin is secured, through a process called proof-of-stake minting. Stakeholders participate in minting simply by installing the version of Peercoin they support, loading their wallet with PPC, and holding them while they are occasionally selected by Peercoin’s protocol to mint the next block in the chain. Every minted block rewards stakeholders with new PPC and a vote.</p>
+            <p>This makes Peercoin the first blockchain capable of allowing its protocol rules to be governed directly by its users, making for a network that is more decentralized, democratic, and easily secured by people around the world.</p>
+          </div>
+        </section>
       </div>
     </div>
     </>
