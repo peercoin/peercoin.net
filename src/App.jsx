acdb141cc86b0976b9717611b0627e76ff04ddaa
blackout <blackout.peercoin@protonmail.com>
2019-09-26 13:05:38 -0400

    Hardcoded myths

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Resources from './pages/Resources/Resources.jsx';
 import Projects from './pages/Projects/Projects';
 import Foundation from './pages/Foundation/Foundation';
 import Contact from './pages/Contact/Contact';
+import Myths from './pages/Myths/Myths';
 import Wallet from './pages/Wallet/Wallet';
 import ScrollTop from './components/ScrollTop/ScrollTop';
 import Privacy from './pages/Privacy/Privacy';
@@ -28,6 +29,7 @@ function App() {
           <Route path="/foundation" component={Foundation} />
           <Route path="/wallet" component={Wallet} />
           <Route path="/contact" component={Contact} />
+          <Route path="/myths" component={Myths} />
         </Switch>
       </>
     </Router>
