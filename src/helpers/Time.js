66239002ca439bce88b989b4b006f1141dae88c9
noface <noface@outlook.com>
2022-02-02 04:29:12 +0100

    formatTime() should return full unit of time
    There is a fair difference between 7m (months) and 7m (minutes).

--- src/helpers/Time.js
+++ src/helpers/Time.js
@@ -1,25 +1,15 @@
-var periods = {
-  month: 30 * 24 * 60 * 60 * 1000,
-  week: 7 * 24 * 60 * 60 * 1000,
-  day: 24 * 60 * 60 * 1000,
-  hour: 60 * 60 * 1000,
-  minute: 60 * 1000
-};
+var periods = [
+  {amount: 30 * 24 * 60 * 60 * 1000, str: "month"},
+  {amount: 7 * 24 * 60 * 60 * 1000, str: "week"},
+  {amount: 24 * 60 * 60 * 1000, str: "day"},
+  {amount: 60 * 60 * 1000, str: "hour"},
+  {amount: 60 * 1000, str: "minute"},
+];
 
 export function formatTime(timeCreated) {
-  var diff = Date.now() - new Date(timeCreated);
-
-  if (diff > periods.month) {
-    return Math.floor(diff / periods.month) + "m ago";
-  } else if (diff > periods.week) {
-    return Math.floor(diff / periods.week) + "w ago";
-  } else if (diff > periods.day) {
-    return Math.floor(diff / periods.day) + "d ago";
-  } else if (diff > periods.hour) {
-    return Math.floor(diff / periods.hour) + "h ago";
-  } else if (diff > periods.minute) {
-    return Math.floor(diff / periods.minute) + "m ago";
-  }
-
-  return "Just now";
+  const diff = Date.now() - new Date(timeCreated);
+  const period = periods.find(({amount}) => diff > amount);
+  if (!period) return 'Just now';
+  const count = Math.floor(diff / period.amount);
+  return `${count} ${period.str}${count > 1 ? 's' : ''} ago`;
 }
