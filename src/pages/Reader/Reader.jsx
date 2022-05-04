77ee9bf0492c5ca37114c2eb8c54a6e8243668ea
Willy <willy@coinerella.com>
2022-05-04 17:59:29 +0200

    embed whitepaper in pdf reader

--- src/pages/Reader/Reader.jsx
+++ src/pages/Reader/Reader.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import Menu from "../../components/Menu/Menu";
 import Footer from "../../components/Footer/Footer";
 import Loader from "../../components/Loader/Loader";
@@ -9,6 +9,21 @@ import { useTranslation } from "react-i18next";
 function Reader() {
   const { folder, file } = useParams();
   const { t } = useTranslation();
+  const [paperAvailable, setPaperAvailable] = useState(false);
+
+  useEffect(() => {
+    fetch("/data/whitepaper.json")
+      .then((res) => res.json())
+      .then((jsonData) => {
+        if (
+          jsonData["resources"].find(
+            (el) => el.url === `/${folder}/${file}`
+          ) !== undefined
+        ) {
+          setPaperAvailable(true);
+        }
+      });
+  }, [file, folder]);
 
   return (
     <>
@@ -17,17 +32,36 @@ function Reader() {
         <h1 className="hero__title">Whitepaper</h1>
       </div>
       <div>
-        <div className="Reader__container">
-          <embed
-            src={`/${folder}/${file}`}
-            type="application/pdf"
-            className="Reader__embed"
-          />
+        <div
+          className={
+            paperAvailable ? "Reader__container" : "Reader__container--fault"
+          }
+        >
+          {paperAvailable ? (
+            <embed
+              src={`/${folder}/${file}`}
+              type="application/pdf"
+              className="Reader__embed"
+            />
+          ) : (
+            <p className="description">{t("reader.notFound")}</p>
+          )}
         </div>
+        {paperAvailable && <p className="description">{t("reader.hint")}</p>}
         <div className="Reader__button">
           <a href="/resources#whitepaper" className="btn btn--green">
             {t("reader.buttonGoBack")}
           </a>
+          {paperAvailable && (
+            <a
+              href={`/${folder}/${file}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="btn btn--green"
+            >
+              {t("reader.buttonDownload")}
+            </a>
+          )}
         </div>
       </div>
       <Footer />
