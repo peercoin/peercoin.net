5368d6be398cb116ae203e13140cf8a013a88cbc
Willy <willy@coinerella.com>
2022-09-26 10:35:40 +0200

    fix latestnews

--- src/components/LatestNews/LatestNews.jsx
+++ src/components/LatestNews/LatestNews.jsx
@@ -2,21 +2,21 @@ import React, { useEffect, useState, Suspense } from "react";
 import "./LatestNews.scss";
 import Loader from "../Loader/Loader";
 import { formatTime } from "../../helpers/Time";
+const { parse } = require("rss-to-json");
 
 function LatestNews() {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     async function getData() {
-      const peercoinPosts = await fetch(
-        "https://api.rss2json.com/v1/api.json?rss_url=https://www.peercoin.net/blog/rss/"
-      ).then((res) => res.json());
+      const peercoinPosts = await parse("https://www.peercoin.net/blog/rss/");
+      console.log(peercoinPosts);
 
       const posts = peercoinPosts.items
-        .sort((a, b) => new Date(b.pubDate) - new Date(a.pubDate))
+        .sort((a, b) => new Date(b.published) - new Date(a.published))
         .map((post) => ({
-          published: post.pubDate,
-          img: post.thumbnail,
+          published: post.published,
+          img: post.media,
           title: post.title,
           link: post.link,
           categories: post.categories,
@@ -40,12 +40,20 @@ function LatestNews() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <img src={post.img} alt={post.title} className="post__img" />
+              <img
+                src={
+                  post.img.hasOwnProperty("thumbnail")
+                    ? post.img.thumbnail.url
+                    : ""
+                }
+                alt={post.title}
+                className="post__img"
+              />
               <h3 className="post__title">{post.title}</h3>
             </a>
 
             <div title={post.published} className="post__published">
-              Published {formatTime(post.published.replace(/ /g, "T"))}
+              Published {formatTime(post.published)}
             </div>
           </div>
         ))}
