210b3b6ae43f9f5474781c7be8037aaae73afbbf
peerchemist <dx.1178@gmail.com>
2023-03-15 13:46:28 +0100

    Update Foundation.jsx
    That too

--- src/pages/Foundation/Foundation.jsx
+++ src/pages/Foundation/Foundation.jsx
@@ -45,7 +45,7 @@ function Foundation() {
                   href="https://chainz.cryptoid.info/ppc/address.dws?p77CZFn9jvg9waCzKBzkQfSvBBzPH1nRre.htm"
                 >
                   {" "}
-                  p92W3t7YkKfQEPDb7cG9jQ6iMh7cpKLvwK
+                  p77CZFn9jvg9waCzKBzkQfSvBBzPH1nRre
                 </a>
                 <br />
                 <Trans i18nKey="foundationPage.generalFundBtcAddress">
